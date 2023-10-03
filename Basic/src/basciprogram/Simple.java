49178b97c9d41b1ff8e472d13d2bd77aeaa9d90a
SHIVA COMPUTERS <SHIVA COMPUTERS@DESKTOP-GF5I5QK>
2023-10-03 09:20:01 +0530

    "code message"

---
+++ Basic/src/basciprogram/Simple.java
@@ -0,0 +1,21 @@
+package basciprogram;
+
+public class Simple {
+	public void studentName() {
+		System.out.println("shree") ;
+	}
+	public void mobileNumber() {
+		System.out.println(848952779);
+	}
+	public void studentId() {
+		System.out.println(2) ;
+	}	
+	
+public static void main(String[]args) {
+	Simple obj = new Simple();
+	obj.studentName();
+	obj.mobileNumber();
+	obj.studentId();
+}
+	
+}
